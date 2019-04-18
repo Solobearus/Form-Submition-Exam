66f92eec6d837cab5941367dec66e92110b43911
ivansolobear <ivansolobear@gmail.com>
2019-04-19 02:13:03 +0300

    changed text in app.js

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -4,7 +4,7 @@ import style from './App.module.css'
 const App = (props) => {
     return (
         <div className={ style.App }>
-            This is a React Redux boilerplate by Ivan Solobear
+            This is a React Redux Material-ui boilerplate by Ivan Solobear
         </div>
     )
 }
